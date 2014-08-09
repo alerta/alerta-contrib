4aafb9f8cbcb09f5cf3a2c9955f33aab8169557c
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-09 22:23:11 +0100

    add influxdb plugin

---
+++ plugins/alerta-influxdb/influxdb.py
@@ -0,0 +1,47 @@
+
+import json
+import requests
+
+from alerta.app import app
+from alerta.plugins import PluginBase
+
+LOG = app.logger
+
+INFLUXDB_URL = 'http://localhost:8086'
+INFLUXDB_USER = 'alerta'
+INFLUXDB_PASSWORD = 'alerta'
+
+
+class InfluxDBWrite(PluginBase):
+
+    def __init__(self):
+
+        self.endpoint = 'http://localhost:8086'
+
+    def pre_receive(self, alert):
+
+        pass
+
+    def post_receive(self, alert):
+
+        url = self.endpoint + '/db/alerta/series'
+
+        data = [
+            {
+                "name": alert.event,
+                "columns": ["value", "environment", "resource"],
+                "points": [
+                    [alert.value, alert.environment, alert.resource]
+                ]
+            }
+        ]
+
+        LOG.debug('InfluxDB data: %s', data)
+
+        try:
+            response = requests.post(url=url, data=json.dumps(data), auth=(INFLUXDB_USER, INFLUXDB_PASSWORD))
+        except Exception as e:
+            raise RuntimeError("InfluxDB connection error: %s", e)
+
+        LOG.debug('InfluxDB response: %s', response)
+
---
+++ plugins/alerta-influxdb/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import setuptools
+
+version = '0.1.0'
+
+setuptools.setup(
+    name="alerta-influxdb",
+    version=version,
+    description='Alerta plugin for InfluxDB',
+    url='https://github.com/alerta/alerta-contrib',
+    license='Apache License 2.0',
+    author='Nick Satterly',
+    author_email='nick.satterly@theguardian.com',
+    py_modules=['influxdb'],
+    install_requires=[
+        'requests',
+        'alerta-server'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'alerta.plugins': [
+            'influxdb = influxdb:InfluxDBWrite'
+        ]
+    }
+)
