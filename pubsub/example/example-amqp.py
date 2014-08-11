7b83af2592134420b069756fe28d9f9b33a4d1d4
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-12 00:11:57 +0100

    fix typos

--- pubsub/example/example-amqp.py
+++ pubsub/example/example-amqp.py
@@ -3,8 +3,8 @@
 from kombu import BrokerConnection, Exchange, Queue
 from kombu.mixins import ConsumerMixin
 
-AMPQ_URL = 'amqp://guest:guest@localhost:5672//'
-AMPQ_QUEUE = 'example'
+AMQP_URL = 'amqp://guest:guest@localhost:5672//'
+AMQP_QUEUE = 'example'
 
 
 class Worker(ConsumerMixin):
@@ -12,8 +12,8 @@ class Worker(ConsumerMixin):
     def __init__(self, connection):
 
         self.connection = connection
-        self.exchange = Exchange(AMPQ_QUEUE, type='direct', durable=True)
-        self.queue = Queue(AMPQ_QUEUE, exchange=self.exchange, routing_key=AMPQ_QUEUE)
+        self.exchange = Exchange(AMQP_QUEUE, type='direct', durable=True)
+        self.queue = Queue(AMQP_QUEUE, exchange=self.exchange, routing_key=AMQP_QUEUE)
 
     def get_consumers(self, Consumer, channel):
 
@@ -32,7 +32,7 @@ def main():
     from kombu.utils.debug import setup_logging
     setup_logging(loglevel='DEBUG')
 
-    with BrokerConnection(AMPQ_URL) as connection:
+    with BrokerConnection(AMQP_URL) as connection:
         try:
             Worker(connection).run()
         except KeyboardInterrupt:
