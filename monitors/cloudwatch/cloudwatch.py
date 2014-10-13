735708f9f5b7ad590a1a30cdb34d368f94f9664c
Nick Satterly <nick.satterly@guardian.co.uk>
2014-10-13 23:11:22 +0100

    update cloudwatch interface

--- monitors/cloudwatch/cloudwatch.py
+++ monitors/cloudwatch/cloudwatch.py
@@ -4,6 +4,7 @@ import sys
 import json
 import time
 import datetime
+import logging
 
 import boto.sqs
 from boto.sqs.message import RawMessage
@@ -13,26 +14,26 @@ from alerta.api import ApiClient
 from alerta.alert import Alert
 from alerta.heartbeat import Heartbeat
 
-__version__ = '3.2.0'
+__version__ = '3.3.0'
 
 AWS_SQS_QUEUE = os.environ.get('AWS_SQS_QUEUE')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.environ.get('AWS_REGION')
 
-ALERTA_ENDPOINT = os.environ.get('ALERTA_ENDPOINT')
-ALERTA_API_KEY = os.environ.get('ALERTA_API_KEY')
-
 AWS_ACCOUNT_ID = {
     '101234567890': 'aws-account-name'
 }
+
+LOG = logging.getLogger("alerta.cloudwatch")
+logging.basicConfig(format="%(asctime)s - %(name)s: %(levelname)s - %(message)s", level=logging.DEBUG)
 
 
 class CloudWatch(object):
 
     def __init__(self):
 
-        self.api = ApiClient(key=ALERTA_API_KEY)
+        self.api = ApiClient()
 
         try:
             connection = boto.sqs.connect_to_region(
@@ -41,50 +42,46 @@ class CloudWatch(object):
                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY
             )
         except boto.exception.SQSError as e:
-            print >>sys.stderr, 'SQS API call failed: %s' % e
+            LOG.error('SQS API call failed: %s', e)
             sys.exit(1)
 
         try:
             self.sqs = connection.create_queue(AWS_SQS_QUEUE)
             self.sqs.set_message_class(RawMessage)
         except boto.exception.SQSError as e:
-            print >>sys.stderr, 'SQS queue error: %s' % e
+            LOG.error('SQS queue error: %s', e)
             sys.exit(1)
 
     def run(self):
 
         while True:
-            print 'Waiting for CloudWatch alarms on %s...' % AWS_SQS_QUEUE
+            LOG.debug('Waiting for CloudWatch alarms on %s...', AWS_SQS_QUEUE)
             try:
-                message = self.sqs.read(wait_time_seconds=20)
+                notification = self.sqs.read(wait_time_seconds=20)
             except boto.exception.SQSError as e:
-                print >>sys.stderr, 'Could not read from queue: %s' % e
+                LOG.warning('Could not read from queue: %s', e)
                 time.sleep(20)
                 continue
 
-            if message:
-                body = message.get_body()
-                cloudwatchAlert = self.parse_notification(body)
+            if notification:
+                cloudwatchAlert = self.parse_notification(notification)
                 try:
                     self.api.send(cloudwatchAlert)
                 except Exception as e:
-                    print >>sys.stderr, 'Failed to send alert: %s' % e
-                self.sqs.delete_message(message)
+                    LOG.warning('Failed to send alert: %s', e)
+                self.sqs.delete_message(notification)
 
-            print 'Send heartbeat...'
-            heartbeat = Heartbeat(origin='alerta-cloudwatch', tags=[__version__])
+            LOG.debug('Send heartbeat...')
+            heartbeat = Heartbeat(tags=[__version__])
             try:
                 self.api.send(heartbeat)
             except Exception as e:
-                print >>sys.stderr, 'Failed to send heartbeat: %s' % e
+                LOG.warning('Failed to send heartbeat: %s', e)
 
-    def parse_notification(self, message):
+    def parse_notification(self, notification):
 
-        notification = json.loads(message)
-        if 'Message' in notification:
-            alarm = json.loads(notification['Message'])
-        else:
-            return
+        notification = json.loads(notification.get_body())
+        alarm = json.loads(notification['Message'])
 
         if 'Trigger' not in alarm:
             return
@@ -94,11 +91,10 @@ class CloudWatch(object):
         event = alarm['AlarmName']
         severity = self.cw_state_to_severity(alarm['NewStateValue'])
         group = 'CloudWatch'
-        value = alarm['AlarmDescription']
-        text = notification['Subject']
-        environment = 'Production'
+        value = alarm['Trigger']['MetricName']
+        text = alarm['AlarmDescription']
         service = [AWS_ACCOUNT_ID.get(alarm['AWSAccountId'], 'AWSAccountId:' + alarm['AWSAccountId'])]
-        tags = [alarm['Trigger']['MetricName'], alarm['Trigger']['Namespace']]
+        tags = [alarm['Trigger']['Namespace']]
         correlate = list()
         origin = notification['TopicArn']
         timeout = None
@@ -112,7 +108,7 @@ class CloudWatch(object):
             group=group,
             value=value,
             severity=severity,
-            environment=environment,
+            environment='Production',
             service=service,
             text=text,
             event_type='cloudwatchAlarm',
@@ -145,8 +141,16 @@ class CloudWatch(object):
 
 def main():
 
+    LOG = logging.getLogger("alerta.cloudwatch")
     try:
         CloudWatch().run()
     except (SystemExit, KeyboardInterrupt):
-        print 'Exiting...'
+        LOG.info("Exiting alerta cloudwatch.")
+        sys.exit(0)
+    except Exception as e:
+        LOG.error(e, exc_info=1)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
 
--- monitors/cloudwatch/setup.py
+++ monitors/cloudwatch/setup.py
@@ -2,14 +2,14 @@
 
 import setuptools
 
-version = '3.2.0'
+version = '3.3.0'
 
 setuptools.setup(
     name="alerta-cloudwatch",
     version=version,
-    description='Alerta daemon for AWS CloudWatch',
+    description='Alerta integration for AWS CloudWatch alarms',
     url='https://github.com/alerta/alerta-contrib',
-    license='MIT',
+    license='Apache License 2.0',
     author='Nick Satterly',
     author_email='nick.satterly@theguardian.com',
     py_modules=['cloudwatch'],
@@ -23,5 +23,9 @@ setuptools.setup(
         'console_scripts': [
             'alerta-cloudwatch = cloudwatch:main'
         ]
-    }
+    },
+    keywords="alerta cloudwatch monitoring",
+    classifiers=[
+        'Topic :: System :: Monitoring',
+    ]
 )
