63e9f4bb90e95069be95408cb97e22a6ce933aba
Moises Silva <msilva@sangoma.com>
2016-01-08 17:27:04 -0500

    Add support to use a jinja2 template for your hipchat message

--- plugins/hipchat/hipchat.py
+++ plugins/hipchat/hipchat.py
@@ -12,6 +12,14 @@ HIPCHAT_URL = 'https://api.hipchat.com/v2'
 HIPCHAT_ROOM = os.environ.get('HIPCHAT_ROOM') or app.config['HIPCHAT_ROOM']  # Room Name or Room API ID
 HIPCHAT_API_KEY = os.environ.get('HIPCHAT_API_KEY') or app.config['HIPCHAT_API_KEY']  # Room Notification Token
 
+HIPCHAT_SUMMARY_TEMPLATE = None
+HIPCHAT_SUMMARY_FMT = os.environ.get('HIPCHAT_SUMMARY_FMT') or app.config.get('HIPCHAT_SUMMARY_FMT', None)  # Message summary format
+if HIPCHAT_SUMMARY_FMT:
+    try:
+        from jinja2 import Template
+        HIPCHAT_SUMMARY_TEMPLATE = Template(HIPCHAT_SUMMARY_FMT)
+    except Exception as e:
+        LOG.error('Jinja template error: {}, template functionality will be unavailable'.format(e))
 
 class SendRoomNotification(PluginBase):
 
@@ -26,10 +34,22 @@ class SendRoomNotification(PluginBase):
 
         url = '%s/room/%s/notification' % (HIPCHAT_URL, HIPCHAT_ROOM)
 
-        summary = "<b>[%s] %s %s - <i>%s on %s</i></b> <a href=\"http://try.alerta.io/#/alert/%s\">%s</a>" % (
-            alert.status.capitalize(), alert.environment, alert.severity.capitalize(), alert.event, alert.resource,
-            alert.id, alert.get_id(short=True)
-        )
+        summary = None
+        if HIPCHAT_SUMMARY_TEMPLATE:
+            try:
+                template_vars = {
+                    'alert': alert,
+                    'config': app.config
+                }
+                summary = HIPCHAT_SUMMARY_TEMPLATE.render(**template_vars)
+            except Exception as e:
+                LOG.error('Summary template rendering error: {}'.format(e))
+
+        if not summary:
+            summary = "<b>[%s] %s %s - <i>%s on %s</i></b> <a href=\"http://try.alerta.io/#/alert/%s\">%s</a>" % (
+                alert.status.capitalize(), alert.environment, alert.severity.capitalize(), alert.event, alert.resource,
+                alert.id, alert.get_id(short=True)
+            )
 
         if alert.severity == 'critical':
             color = "red"
@@ -56,6 +76,7 @@ class SendRoomNotification(PluginBase):
             'Content-type': 'application/json'
         }
 
+        LOG.debug('HipChat sending notification to %s', url)
         try:
             r = requests.post(url, data=json.dumps(payload), headers=headers, timeout=2)
         except Exception as e:
