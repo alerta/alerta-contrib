223ebfddd00bb50451f8f3daa78fee5241cb18c7
root <root@ubu001.pod01.secdo.net>
2016-06-26 06:45:42 +0000

    some cleanup, added heartbeat script for cron

--- integrations/consul/consul-heartbeat.py
+++ integrations/consul/consul-heartbeat.py
@@ -2,6 +2,7 @@
 
 from alerta.api import ApiClient
 from alerta.alert import Alert
+from alerta.heartbeat import Heartbeat
 import sys
 import os
 import time
@@ -11,9 +12,6 @@ import time
 
 client = consul.Consul(host='127.0.0.1', port=8500, token=None, scheme='http', consistency='default', dc=None, verify=True)
 
-j = json.load(sys.stdin)
-print j
-
 url = client.kv.get('alerta/apiurl')[1]['Value']
 key = client.kv.get('alerta/apikey')[1]['Value']
 
@@ -22,33 +20,24 @@ sleep = int(client.kv.get('alerta/sleep')[1]['Value'])
 timeout = int(client.kv.get('alerta/timeout')[1]['Value'])
 
 origin = client.kv.get('alerta/origin')[1]['Value']
-alerttype = client.kv.get('alerta/alerttype')[1]['Value']
 api = ApiClient(endpoint=url, key=key)
 
-SEVERITY_MAP = {
-    'critical':   'critical',
-    'warning':    'warning',
-    'passing':    'ok',
-}
-
-def createalert( data ):
-    alert = Alert(resource=data['Node'], event='Problem', environment='Production', service=[data['CheckId']], severity=SEVERITY_MAP[data['Status']], text=data['Output'], value='Error', timeout=timeout, origin=origin, type=alerttype)
+def createheartbeat():
+    hb = Heartbeat(origin=origin, timeout=timeout)
     for i in range(max_retries):
         try:
-            print(api.send(alert))
+            print(api.send(hb))
         except Exception as e:
             print("HTTP Error: {}".format(e))
             time.sleep(sleep)
-            continue  
+            continue
         else:
             break
     else:
         print("api is down")
 
 def main():
-    for item in enumerate(j):
-        i=item[0]
-        createalert(j[i])
+    createheartbeat()
 
 if __name__ == "__main__":
     main()
