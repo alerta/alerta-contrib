3b0cce947734650e1be49fb6e8d50e56f0f2acb0
root <root@ubu001.pod01.secdo.net>
2016-11-08 11:58:12 +0000

    removed value, no use here

--- integrations/consul/consul-alerta.py
+++ integrations/consul/consul-alerta.py
@@ -25,10 +25,7 @@ timeout = int(client.kv.get('alerta/timeout')[1]['Value'])
 origin = client.kv.get('alerta/origin')[1]['Value']
 alerttype = client.kv.get('alerta/alerttype')[1]['Value']
 
-value = 'Problem'
-
 api = ApiClient(endpoint=url, key=key)
-
 
 SEVERITY_MAP = {
     'critical':   'critical',
@@ -41,7 +38,7 @@ def createalert( data ):
         environment = client.kv.get('alerta/env/{0}'.format(data['Node']))[1]['Value']
     except:
         environment = client.kv.get('alerta/defaultenv')[1]['Value']
-    alert = Alert(resource=data['Node'], event=data['Status'], environment=environment, service=[data['CheckId']], severity=SEVERITY_MAP[data['Status']], text=data['Output'], value=value, timeout=timeout, origin=origin, type=alerttype)
+    alert = Alert(resource=data['Node'], event=data['Status'], environment=environment, service=[data['CheckId']], severity=SEVERITY_MAP[data['Status']], text=data['Output'], timeout=timeout, origin=origin, type=alerttype)
     for i in range(max_retries):
         try:
             print("Response:")
