1fcff4d663301e1de9391411a1259e5887abfeaa
Nick Satterly <nick.satterly@theguardian.com>
2016-11-19 21:23:47 +0000

    Delete silences in Prometheus Alertmanager

--- plugins/prometheus/alerta_prometheus.py
+++ plugins/prometheus/alerta_prometheus.py
@@ -1,16 +1,18 @@
 
+import os
 import datetime
 import requests
 import logging
 
 from alerta.app import app
+from alerta.app import db
 from alerta.plugins import PluginBase
 
 LOG = logging.getLogger('alerta.plugins.prometheus')
 
-ALERTMANAGER_API_URL = 'http://localhost:9093'
-ALERTMANAGER_API_KEY = ''  # not used
-ALERTMANAGER_SILENCE_DAYS = 1
+ALERTMANAGER_API_URL = os.environ.get('ALERTMANAGER_API_URL') or app.config.get('ALERTMANAGER_API_URL', 'http://localhost:9093')
+ALERTMANAGER_API_KEY = os.environ.get('ALERTMANAGER_API_KEY') or app.config.get('ALERTMANAGER_API_KEY', '')  # not used
+ALERTMANAGER_SILENCE_DAYS = os.environ.get('ALERTMANAGER_SILENCE_DAYS') or app.config.get('ALERTMANAGER_SILENCE_DAYS', 1)
 
 
 class AlertmanagerSilence(PluginBase):
@@ -22,6 +24,7 @@ class AlertmanagerSilence(PluginBase):
         return
 
     def status_change(self, alert, status, text):
+
         if alert.event_type != 'prometheusAlert':
             return
 
@@ -29,9 +32,7 @@ class AlertmanagerSilence(PluginBase):
             return
 
         if status == 'ack':
-
-            url = ALERTMANAGER_API_URL + '/api/v1/silences'
-
+            LOG.debug('Alertmanager: Add silence for alertname=%s instance=%s', alert.event, alert.resource)
             data = {
                 "matchers": [
                     {
@@ -49,12 +50,36 @@ class AlertmanagerSilence(PluginBase):
                 "createdBy": "alerta",
                 "comment": text if text != '' else "silenced by alerta"
             }
-            LOG.debug('Alertmanager payload: %s', data)
 
-            LOG.debug('Alertmanager sending silence request to %s', url)
+            url = ALERTMANAGER_API_URL + '/api/v1/silences'
             try:
                 r = requests.post(url, json=data, timeout=2)
             except Exception as e:
-                raise RuntimeError("Alertmanager connection error: %s", e)
+                raise RuntimeError("Alertmanager: ERROR - %s", e)
+            LOG.debug('Alertmanager: %s - %s', r.status_code, r.text)
 
-            LOG.debug('Alertmanager response: %s - %s', r.status_code, r.text)
+            # example r={"status":"success","data":{"silenceId":8}}
+            try:
+                silenceId = r.json()['data']['silenceId']
+                db.update_attributes(alert.id, {'silenceId': silenceId})
+            except Exception as e:
+                raise RuntimeError("Alertmanager: ERROR - %s", e)
+            LOG.debug('Alertmanager: Added silenceId %s to attributes', silenceId)
+
+        elif status == 'open':
+            LOG.debug('Alertmanager: Remove silence for alertname=%s instance=%s', alert.event, alert.resource)
+
+            silenceId = alert.attributes.get('silenceId', None)
+            if silenceId:
+                url = ALERTMANAGER_API_URL + '/api/v1/silence/%s' % silenceId
+                try:
+                    r = requests.delete(url, timeout=2)
+                except Exception as e:
+                    raise RuntimeError("Alertmanager: ERROR - %s", e)
+                LOG.debug('Alertmanager: %s - %s', r.status_code, r.text)
+
+                try:
+                    db.update_attributes(alert.id, {'silenceId': None})
+                except Exception as e:
+                    raise RuntimeError("Alertmanager: ERROR - %s", e)
+                LOG.debug('Alertmanager: Removed silenceId %s from attributes', silenceId)
