c2523ca123c392ad64d370c121a025297f45ca72
Konstantinos Kagkelidis <kaggis@gmail.com>
2017-10-27 03:57:11 +0300

    Publish utc-aware datetimes in AMQP plugin

--- plugins/amqp/setup.py
+++ plugins/amqp/setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup, find_packages
 
-version = '5.4.0'
+version = '5.4.1'
 
 setup(
     name="alerta-amqp",
@@ -14,7 +14,8 @@ setup(
     packages=find_packages(),
     py_modules=['alerta_amqp'],
     install_requires=[
-        'kombu'
+        'kombu',
+        'pytz'
     ],
     include_package_data=True,
     zip_safe=True,
