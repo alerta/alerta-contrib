d8a49cd6eddec43a282a335d663b22355bf5a113
Anton <anton@trugen.net>
2018-04-02 12:34:49 +0200

    Support for sending notifications to MS Teams (#180)
    * Initial version of MS Teams plugin
    
    * Removed old debug
    
    * Readme and minor changes
    
    * Changed name of web hook url.
    
    * Renamed webhook parameter

---
+++ plugins/msteams/README.md
@@ -0,0 +1,60 @@
+Microsoft Teams Plugin
+==============
+
+Send Microsoft Teams messages for new alerts.
+
+If you have the Python Jinja2 package installed you can customize the
+Microsoft Teams message format.
+
+For help, join [![Gitter chat](https://badges.gitter.im/alerta/chat.png)](https://gitter.im/alerta/chat)
+
+Installation
+------------
+
+Clone the GitHub repo and run:
+
+    $ python setup.py install
+
+Or, to install remotely from GitHub run:
+
+    $ pip install git+https://github.com/alerta/alerta-contrib.git#subdirectory=plugins/msteams
+
+Note: If Alerta is installed in a python virtual environment then plugins
+need to be installed into the same environment for Alerta to dynamically
+discover them.
+
+Configuration
+-------------
+
+Add `msteams` to the list of enabled `PLUGINS` in `alertad.conf` server
+configuration file and set plugin-specific variables either in the
+server configuration file or as environment variables.
+
+```python
+PLUGINS = ['msteams']
+MS_TEAMS_WEBHOOK_URL =  'https://outlook.office.com/webhook/.../IncomingWebhook/.../...'
+DASHBOARD_URL = 'http://try.alerta.io'
+```
+
+The `MS_TEAMS_SUMMARY_FMT` configuration variable is a Jinja2 template
+string and accepts any Jinja2 syntax. The formatter has access to two
+variables in the template environment, 'alert' for all alert details
+and 'config' for access to the alerta configuration.
+
+If you have Jinja2 available you can try customizing the message like
+this:
+
+```python
+MS_TEAMS_SUMMARY_FMT = '<b>[{{ alert.status|capitalize }}]</b> [{{ alert.severity|upper }}] Event {{ alert.event }} on <b>{{ alert.resource }}</b><br>{{ alert.text }}'
+```
+
+References
+----------
+
+  * HipChat room notification API: https://github.com/rveachkc/pymsteams
+  * Jinja2 templating language for Python: http://jinja.pocoo.org/docs/dev/
+
+License
+-------
+
+Copyright (c) 2017 Anton Delitsch. Available under the MIT License.
---
+++ plugins/msteams/alerta_msteams.py
@@ -0,0 +1,86 @@
+
+import json
+import logging
+import os
+import pymsteams
+
+try:
+    from alerta.plugins import app  # alerta >= 5.0
+except ImportError:
+    from alerta.app import app  # alerta < 5.0
+from alerta.plugins import PluginBase
+
+LOG = logging.getLogger('alerta.plugins.msteams')
+
+MS_TEAMS_WEBHOOK_URL = os.environ.get('MS_TEAMS_WEBHOOK_URL') or app.config.get('MS_TEAMS_WEBHOOK_URL')
+MS_TEAMS_SUMMARY_FMT = os.environ.get('MS_TEAMS_SUMMARY_FMT') or app.config.get('MS_TEAMS_SUMMARY_FMT', None)  # Message summary format
+DASHBOARD_URL = os.environ.get('DASHBOARD_URL') or app.config.get('DASHBOARD_URL', '')
+
+try:
+    from jinja2 import Template
+except Exception as e:
+    LOG.error('MS Teams: ERROR - Jinja template error: %s, template functionality will be unavailable', e)
+
+
+class SendConnectorCardMessage(PluginBase):
+
+    def pre_receive(self, alert):
+        return alert
+
+    def post_receive(self, alert):
+
+        if alert.repeat:
+            return
+
+        if MS_TEAMS_SUMMARY_FMT:
+            try:
+                template = Template(MS_TEAMS_SUMMARY_FMT)
+            except Exception as e:
+                LOG.error('MS Teams: ERROR - Template init failed: %s', e)
+                return
+
+            try:
+                template_vars = {
+                    'alert': alert,
+                    'config': app.config
+                }
+                summary = template.render(**template_vars)
+            except Exception as e:
+                LOG.error('MS Teams: ERROR - Template render failed: %s', e)
+                return
+        else:
+            summary = ('<b>[{status}] {environment} {service} {severity} - <i>{event} on {resource}</i></b>').format(
+                status=alert.status.capitalize(),
+                environment=alert.environment.upper(),
+                service=','.join(alert.service),
+                severity=alert.severity.capitalize(),
+                event=alert.event,
+                resource=alert.resource
+            )
+            url = "%s/#/alert/%s" % (DASHBOARD_URL, alert.id)
+            
+        if alert.severity == 'critical':
+            color = "D8122A"
+        elif alert.severity == 'major':
+            color = "EA680F"
+        elif alert.severity == 'minor':
+            color = "FFBE1E"
+        elif alert.severity == 'warning':
+            color = "BA2222"
+        else:
+            color = "00AA5A"
+
+        LOG.debug('MS Teams payload: %s', summary)
+
+        try:
+            msTeamsMessage = pymsteams.connectorcard(MS_TEAMS_WEBHOOK_URL)
+            msTeamsMessage.title(summary)
+            msTeamsMessage.text(alert.text)
+            msTeamsMessage.addLinkButton("Open in Alerta", url)
+            msTeamsMessage.color(color)
+            msTeamsMessage.send()
+        except Exception as e:
+            raise RuntimeError("MS Teams: ERROR - %s", e)
+
+    def status_change(self, alert, status, text):
+        return
