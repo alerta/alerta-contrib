0cc3a73f513987a68a4c5c6e7271272f69d05b26
SK <skob@me.com>
2018-04-26 00:12:02 +0300

    forward plugin: [fix] ability to configure via env + refactor via @satterly comment

--- plugins/forward/alerta_forward.py
+++ plugins/forward/alerta_forward.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 from alerta.plugins import PluginBase
@@ -10,9 +11,12 @@ except ImportError:
 
 LOG = logging.getLogger('alerta.plugins.forward')
 
-FORWARD_URL = app.config.get('FORWARD_URL')
-FORWARD_API_KEY = app.config.get('FORWARD_API_KEY')
-FORWARD_MAX_LENGTH = app.config.get('FORWARD_MAX_LENGTH') or 3
+FORWARD_URL = os.environ.get(
+    'FORWARD_URL') or app.config.get('FORWARD_URL')
+FORWARD_API_KEY = os.environ.get(
+    'FORWARD_API_KEY') or app.config.get('FORWARD_API_KEY')
+FORWARD_MAX_LENGTH = os.environ.get(
+    'FORWARD_MAX_LENGTH') or app.config.get('FORWARD_MAX_LENGTH') or 3
 
 class ForwardAlert(PluginBase):
 
@@ -23,20 +27,15 @@ class ForwardAlert(PluginBase):
         if not FORWARD_URL or not FORWARD_API_KEY:
             return
         client = Client(endpoint=FORWARD_URL, key=FORWARD_API_KEY)
-        fw_alert = alert.serialize
-        event = fw_alert.pop('event')
-        resource = fw_alert.pop('resource')
         fw_count = alert.attributes.get('fw_count') or 0
         fw_count = fw_count+1
         if fw_count >= FORWARD_MAX_LENGTH:
             LOG.debug('alert discarded by cycle overflow')
             return
 
-        fw_alert.get('attributes')['fw_count'] = fw_count
+        alert.get('attributes')['fw_count'] = fw_count
         client.send_alert(
-            event=event,
-            resource=resource,
-            **fw_alert
+            **alert
         )
         return
 
