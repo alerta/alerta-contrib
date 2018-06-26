b2a74f626143296f65e393d56ce3c2f908bf7ba1
Kurt Westerfeld <kwesterfeld@users.noreply.github.com>
2018-06-26 16:36:03 -0400

    Update the opsgenie integration for the v2 REST API.  The opsgenie v1 JSON API is defunct as of 06/30/2018 (#197)

--- plugins/opsgenie/alerta_opsgenie.py
+++ plugins/opsgenie/alerta_opsgenie.py
@@ -13,8 +13,8 @@ from alerta.plugins import PluginBase
 LOG = logging.getLogger('alerta.plugins.opsgenie')
 LOG.info('Initializing')
 
-OPSGENIE_EVENTS_CREATE_URL = 'https://api.opsgenie.com/v1/json/alert'
-OPSGENIE_EVENTS_CLOSE_URL = 'https://api.opsgenie.com/v1/json/alert/close'
+OPSGENIE_EVENTS_CREATE_URL = 'https://api.opsgenie.com/v2/alerts'
+OPSGENIE_EVENTS_CLOSE_URL = 'https://api.opsgenie.com/v2/alerts/%s/close?identifierType=alias'
 OPSGENIE_SERVICE_KEY = os.environ.get('OPSGENIE_SERVICE_KEY') or app.config['OPSGENIE_SERVICE_KEY']
 SERVICE_KEY_MATCHERS = os.environ.get('SERVICE_KEY_MATCHERS') or app.config['SERVICE_KEY_MATCHERS']
 DASHBOARD_URL = os.environ.get('DASHBOARD_URL') or app.config.get('DASHBOARD_URL', '')
@@ -37,15 +37,15 @@ class TriggerEvent(PluginBase):
 
     def opsgenie_close_alert(self, alert, why):
 
-        payload = {
-            "apiKey": self.opsgenie_service_key(alert.resource),
-            "alias": alert.id
+        headers = {
+            "Authorization": 'GenieKey ' + self.opsgenie_service_key(alert.resource)
         }
 
-        LOG.debug('OpsGenie %s payload: %s' % (why, payload))
+        closeUrl = OPSGENIE_EVENTS_CLOSE_URL % alert.id
+        LOG.debug('OpsGenie close %s: %s %s' % (why, alert.id, closeUrl))
 
         try:
-            r = requests.post(OPSGENIE_EVENTS_CLOSE_URL, json=payload, timeout=2)
+            r = requests.post(closeUrl, json={}, headers=headers, timeout=2)
         except Exception as e:
             raise RuntimeError("OpsGenie connection error: %s" % e)
         return r
@@ -64,33 +64,46 @@ class TriggerEvent(PluginBase):
             r = self.opsgenie_close_alert(alert, 'CREATE-CLOSE')
 
         else:
+            headers = {
+                "Authorization": 'GenieKey ' + self.opsgenie_service_key(alert.resource)
+            }
+
             # Send all alert data as details to opsgenie
-            details = alert.get_body(history=False)
+            body = alert.get_body(history=False)
+            details = {}
             details['web_url'] = '%s/#/alert/%s' % (DASHBOARD_URL, alert.id)
+            details['service'] = alert.service[0]
+            details['origin'] = body['origin']
+            details['event'] = body['event']
+            details['group'] = body['group']
+            details['trendIndication'] = body['trendIndication']
+            details['severity'] = body['severity']
+            details['previousSeverity'] = body['previousSeverity']
+            details['duplicateCount'] = body['duplicateCount']
 
             payload = {
-                "apiKey": self.opsgenie_service_key(alert.resource),
                 "alias": alert.id,
                 "message": "[ %s ]: %s: %s" % (alert.environment, alert.severity, alert.text),
                 "entity": alert.environment,
                 "teams" : [],
-                "tags": "%s,%s,%s,%s" % (alert.environment, alert.resource, alert.service[0], alert.event),
+                "tags": [alert.environment, alert.resource, alert.service[0], alert.event],
                 "details": details
             }
 
-            LOG.debug('OpsGenie CREATE payload: %s', payload)
+            LOG.debug('OpsGenie CREATE payload: %s' % payload)
 
             try:
-                r = requests.post(OPSGENIE_EVENTS_CREATE_URL, json=payload, timeout=2)
+                r = requests.post(OPSGENIE_EVENTS_CREATE_URL, json=payload, headers=headers, timeout=2)
             except Exception as e:
                 raise RuntimeError("OpsGenie connection error: %s" % e)
 
         LOG.debug('OpsGenie response: %s - %s', r.status_code, r.text)
 
     def status_change(self, alert, status, text):
-        LOG.debug('Alert change %s: %s' % (alert.id, alert.get_body(history=False)))
+        LOG.debug('Alert change %s to %s: %s' % (alert.id, status, alert.get_body(history=False)))
 
-        if status not in ['ack', 'assign']:
+        if status not in ['ack', 'assign', 'closed']:
+            LOG.debug('Not sending status change to opsgenie: %s to %s' % (alert.id, status)) 
             return
 
         r = self.opsgenie_close_alert(alert, 'STATUS-CLOSE')
