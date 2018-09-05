9a7251d9ee7d8447e8bd804d53b58da7911bf141
Nick Satterly <nick.satterly@gmail.com>
2018-09-05 17:00:58 +0200

    Whitespace and bump version

--- plugins/slack/alerta_slack.py
+++ plugins/slack/alerta_slack.py
@@ -1,64 +1,64 @@
-
 import json
 import logging
 import os
+
 import requests
+
+LOG = logging.getLogger('alerta.plugins.slack')
 
 try:
     from jinja2 import Template
 except Exception as e:
-    LOG.error('SLACK: ERROR - Jinja template error: %s, template functionality will be unavailable', e)
+    LOG.warning('Slack Jinja template error: %s, template functionality will be unavailable', e)
 
 try:
     from alerta.plugins import app  # alerta >= 5.0
 except ImportError:
     from alerta.app import app  # alerta < 5.0
-
 from alerta.plugins import PluginBase
 
-LOG = logging.getLogger('alerta.plugins.slack')
+SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL') or app.config['SLACK_WEBHOOK_URL']
+SLACK_ATTACHMENTS = True if os.environ.get('SLACK_ATTACHMENTS', 'False') == 'True' else app.config.get(
+    'SLACK_ATTACHMENTS', False)
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL') or app.config.get('SLACK_CHANNEL', '')
+SLACK_CHANNEL_ENV_MAP = os.environ.get('SLACK_CHANNEL_ENV_MAP') or app.config.get('SLACK_CHANNEL_ENV_MAP', {})
+ALERTA_USERNAME = os.environ.get('ALERTA_USERNAME') or app.config.get('ALERTA_USERNAME', 'alerta')
 
-SLACK_WEBHOOK_URL = os.environ.get(
-    'SLACK_WEBHOOK_URL') or app.config['SLACK_WEBHOOK_URL']
-SLACK_ATTACHMENTS = True if os.environ.get(
-    'SLACK_ATTACHMENTS', 'False') == 'True' else app.config.get('SLACK_ATTACHMENTS', False)
-SLACK_CHANNEL = os.environ.get(
-    'SLACK_CHANNEL') or app.config.get('SLACK_CHANNEL', '')
-SLACK_CHANNEL_ENV_MAP = os.environ.get(
-    'SLACK_CHANNEL_ENV_MAP') or app.config.get('SLACK_CHANNEL_ENV_MAP', dict())
-ALERTA_USERNAME = os.environ.get(
-    'ALERTA_USERNAME') or app.config.get('ALERTA_USERNAME', 'alerta')
-SLACK_SEND_ON_ACK = os.environ.get(
-    'SLACK_SEND_ON_ACK') or app.config.get('SLACK_SEND_ON_ACK', False)
+SLACK_SEND_ON_ACK = os.environ.get('SLACK_SEND_ON_ACK') or app.config.get('SLACK_SEND_ON_ACK', False)
 SLACK_SEVERITY_MAP = app.config.get('SLACK_SEVERITY_MAP', {})
-SLACK_DEFAULT_SEVERITY_MAP = {'security': '#000000', # black
-                              'critical': '#FF0000', # red
-                              'major': '#FFA500', # orange
-                              'minor': '#FFFF00', # yellow
-                              'warning': '#1E90FF', #blue
-                              'informational': '#808080', #gray
-                              'debug': '#808080', # gray
-                              'trace': '#808080', # gray
-                              'ok': '#00CC00'} # green
-SLACK_SUMMARY_FMT = os.environ.get('SLACK_SUMMARY_FMT') or app.config.get('SLACK_SUMMARY_FMT', None)  # Message summary format
-SLACK_DEFAULT_SUMMARY_FMT='<b>[{status}] {environment} {service} {severity} - <i>{event} on {resource}</i></b> <a href="{dashboard}/#/alert/{alert_id}">{short_id}</a>'
+SLACK_DEFAULT_SEVERITY_MAP = {
+    'security': '#000000',  # black
+    'critical': '#FF0000',  # red
+    'major': '#FFA500',  # orange
+    'minor': '#FFFF00',  # yellow
+    'warning': '#1E90FF',  # blue
+    'informational': '#808080',  # gray
+    'debug': '#808080',  # gray
+    'trace': '#808080',  # gray
+    'ok': '#00CC00'  # green
+}
+
+SLACK_SUMMARY_FMT = os.environ.get('SLACK_SUMMARY_FMT') or app.config.get('SLACK_SUMMARY_FMT',
+                                                                          None)  # Message summary format
+SLACK_DEFAULT_SUMMARY_FMT = '<b>[{status}] {environment} {service} {severity} - <i>{event} on {resource}</i></b> <a href="{dashboard}/#/alert/{alert_id}">{short_id}</a>'
 SLACK_DEFAULT_ATTACHMENT_SIZE_MAP = {
-                              'resource': False,
-                              'severity': True,
-                              'environment': True,
-                              'status': True,
-                              'services': True}
+    'resource': False,
+    'severity': True,
+    'environment': True,
+    'status': True,
+    'services': True
+}
 SLACK_ATTACHMENT_SIZE_MAP = app.config.get('SLACK_ATTACHMENT_SIZE_MAP', SLACK_DEFAULT_ATTACHMENT_SIZE_MAP)
-ICON_EMOJI = os.environ.get('ICON_EMOJI') or app.config.get(
-    'ICON_EMOJI', ':rocket:')
-DASHBOARD_URL = os.environ.get(
-    'DASHBOARD_URL') or app.config.get('DASHBOARD_URL', '')
+ICON_EMOJI = os.environ.get('ICON_EMOJI') or app.config.get('ICON_EMOJI', ':rocket:')
+DASHBOARD_URL = os.environ.get('DASHBOARD_URL') or app.config.get('DASHBOARD_URL', '')
+
 SLACK_HEADERS = {
     'Content-Type': 'application/json'
 }
-SLACK_TOKEN = os.environ.get('SLACK_TOKEN') or app.config['SLACK_TOKEN']
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN') or app.config.get('SLACK_TOKEN', None)
 if SLACK_TOKEN:
     SLACK_HEADERS['Authorization'] = 'Bearer ' + SLACK_TOKEN
+
 
 class ServiceIntegration(PluginBase):
 
@@ -127,14 +127,31 @@ class ServiceIntegration(PluginBase):
                 "attachments": [{
                     "color": color,
                     "fields": [
-                        {"title": "Resource", "value": alert.resource, "short": SLACK_ATTACHMENT_SIZE_MAP['resource']},
-                        {"title": "Severity", "value": alert.severity.capitalize(), "short": SLACK_ATTACHMENT_SIZE_MAP['severity']},
-                        {"title": "Status", "value": (status if status else alert.status).capitalize(),
-                         "short": SLACK_ATTACHMENT_SIZE_MAP['status']},
-                        {"title": "Environment",
-                            "value": alert.environment, "short": SLACK_ATTACHMENT_SIZE_MAP['environment']},
-                        {"title": "Services", "value": ", ".join(
-                            alert.service), "short": SLACK_ATTACHMENT_SIZE_MAP['services']}
+                        {
+                            "title": "Resource",
+                            "value": alert.resource,
+                            "short": SLACK_ATTACHMENT_SIZE_MAP['resource']
+                        },
+                        {
+                            "title": "Severity",
+                            "value": alert.severity.capitalize(),
+                            "short": SLACK_ATTACHMENT_SIZE_MAP['severity']
+                        },
+                        {
+                            "title": "Status",
+                            "value": (status if status else alert.status).capitalize(),
+                            "short": SLACK_ATTACHMENT_SIZE_MAP['status']
+                        },
+                        {
+                            "title": "Environment",
+                            "value": alert.environment,
+                            "short": SLACK_ATTACHMENT_SIZE_MAP['environment']
+                        },
+                        {
+                            "title": "Services",
+                            "value": ", ".join(alert.service),
+                            "short": SLACK_ATTACHMENT_SIZE_MAP['services']
+                        }
                     ]
                 }]
             }
@@ -147,12 +164,10 @@ class ServiceIntegration(PluginBase):
             return
 
         payload = self._slack_prepare_payload(alert)
-
         LOG.debug('Slack payload: %s', payload)
 
         try:
-            r = requests.post(SLACK_WEBHOOK_URL,
-                              data=json.dumps(payload), headers=SLACK_HEADERS, timeout=2)
+            r = requests.post(SLACK_WEBHOOK_URL, json=payload, headers=SLACK_HEADERS, timeout=2)
         except Exception as e:
             raise RuntimeError("Slack connection error: %s", e)
 
@@ -163,11 +178,10 @@ class ServiceIntegration(PluginBase):
             return
 
         payload = self._slack_prepare_payload(alert, status, text)
-
         LOG.debug('Slack payload: %s', payload)
+
         try:
-            r = requests.post(SLACK_WEBHOOK_URL,
-                              data=json.dumps(payload), headers=SLACK_HEADERS, timeout=2)
+            r = requests.post(SLACK_WEBHOOK_URL, json=payload, headers=SLACK_HEADERS, timeout=2)
         except Exception as e:
             raise RuntimeError("Slack connection error: %s", e)
 
