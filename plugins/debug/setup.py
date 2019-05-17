b218de65b6cad3bb9a82bdeddd160f452773528d
Nick Satterly <nfsatterly@gmail.com>
2019-05-18 00:03:16 +0200

    Add debug plugin (#263)

---
+++ plugins/debug/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+version = '7.0.0'
+
+setup(
+    name="alerta-debug",
+    version=version,
+    description='Alerta plugin for debug & tracing',
+    url='https://github.com/alerta/alerta-contrib',
+    license='MIT',
+    author='Nick Satterly',
+    author_email='nick.satterly@gmail.com',
+    packages=find_packages(),
+    py_modules=['alerta_debug'],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'alerta.plugins': [
+            'debug = alerta_debug:DebugTracing'
+        ]
+    },
+    python_requires='>=3.5'
+)
