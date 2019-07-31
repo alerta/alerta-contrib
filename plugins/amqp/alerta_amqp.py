b8501dd5963e4013155960b7383953485c65b3bc
Yann Cézard <ycezard@viareport.com>
2019-07-31 12:12:22 +0200

    Use PluginBase get_config to do thye job.

--- plugins/amqp/alerta_amqp.py
+++ plugins/amqp/alerta_amqp.py
@@ -20,11 +20,6 @@ DEFAULT_AMQP_SEND_ALERT_HISTORY = True
 AMQP_URL = os.environ.get('REDIS_URL') or os.environ.get('AMQP_URL') or app.config.get('AMQP_URL', DEFAULT_AMQP_URL)
 AMQP_TOPIC = os.environ.get('AMQP_TOPIC') or app.config.get('AMQP_TOPIC', DEFAULT_AMQP_TOPIC)
 
-if "AMQP_SEND_ALERT_HISTORY" in os.environ:
-    AMQP_SEND_ALERT_HISTORY = False if os.environ.get("AMQP_SEND_ALERT_HISTORY") == "False" else True
-else:
-    AMQP_SEND_ALERT_HISTORY = app.config.get('AMQP_SEND_ALERT_HISTORY', DEFAULT_AMQP_SEND_ALERT_HISTORY)
-
 
 class FanoutPublisher(PluginBase):
 
@@ -49,14 +44,14 @@ class FanoutPublisher(PluginBase):
 
         LOG.info('Configured fanout publisher on topic "%s"', AMQP_TOPIC)
 
-    def pre_receive(self, alert):
+    def pre_receive(self, alert, **kwargs):
         return alert
 
-    def post_receive(self, alert):
+    def post_receive(self, alert, **kwargs):
         LOG.info('Sending message %s to AMQP topic "%s"', alert.get_id(), AMQP_TOPIC)
-        body = alert.get_body(history=AMQP_SEND_ALERT_HISTORY)
+        body = alert.get_body(history=self.get_config('AMQP_SEND_ALERT_HISTORY', default=DEFAULT_AMQP_SEND_ALERT_HISTORY, type=bool, **kwargs))
         LOG.debug('Message: %s', body)
         self.producer.publish(body, declare=[self.exchange], retry=True)
 
-    def status_change(self, alert, status, text):
+    def status_change(self, alert, status, text, **kwargs):
         return
