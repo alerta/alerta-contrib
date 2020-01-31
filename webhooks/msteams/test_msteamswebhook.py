6174ad1be55395fe3dab1582ad3f7275437213fe
Jarno Huuskonen <jarno.huuskonen@uef.fi>
2020-01-31 10:08:29 +0200

    msteams webhook: comment tests that need mocking

--- webhooks/msteams/test_msteamswebhook.py
+++ webhooks/msteams/test_msteamswebhook.py
@@ -7,7 +7,6 @@ from uuid import uuid4
 
 import alerta_msteamswebhook
 
-
 class MsteamsWebhookTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -18,27 +17,17 @@ class MsteamsWebhookTestCase(unittest.TestCase):
         }
         self.app = create_app(test_config)
         self.client = self.app.test_client()
+        custom_webhooks.webhooks['msteams'] = alerta_msteamswebhook.MsteamsWebhook()
 
-        self.example_alert = {
-            'event': 'node_down',
-            'resource': str(uuid4()).upper()[:8],
-            'environment': 'Production',
-            'service': ['Network'],
-            'severity': 'critical',
-            'correlate': ['node_down', 'node_marginal', 'node_up'],
-            'tags': ['foo'],
-            'attributes': {'foo': 'abc def', 'bar': 1234, 'baz': False}
-        }
         self.headers = {
             'X-API-Key': 'some_secret_string',
             'Authentication': 'Bearer fakestring-here',
             'Content-Type': 'application/json'
         }
 
+        self.alert_id = 'f0c55228-c61d-462a-9aeb-f6048d37fdf6'
 
     def test_msteamswebhook(self):
-
-        custom_webhooks.webhooks['msteams'] = alerta_msteamswebhook.MsteamsWebhook()
 
         payload_cmd = """
             {
@@ -63,19 +52,14 @@ class MsteamsWebhookTestCase(unittest.TestCase):
             }
         """
 
-        # Create alert for tests
-        response = self.client.post('/alert', data=json.dumps(self.example_alert), headers=self.headers)
-        self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data.decode('utf-8'))
-        alert_id = data['id']
-
         # ack
-        response = self.client.post('/webhooks/msteams', data=payload_cmd % ('ack', alert_id), content_type='application/json', headers=self.headers)
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data['status'], 'ok')
-        self.assertEqual(data['message'], 'status changed')
-        self.assertTrue(bool(response.headers.get('CARD-ACTION-STATUS', False)))
+        # TODO: needs to mock Alert.find_by_id / alert.set_status
+        #response = self.client.post('/webhooks/msteams', data=payload_cmd % ('ack', self.alert_id), content_type='application/json', headers=self.headers)
+        #self.assertEqual(response.status_code, 200)
+        #data = json.loads(response.data.decode('utf-8'))
+        #self.assertEqual(data['status'], 'ok')
+        #self.assertEqual(data['message'], 'status changed')
+        #self.assertTrue(bool(response.headers.get('CARD-ACTION-STATUS', False)))
 
         # ack with missing alert_id
         response = self.client.post('/webhooks/msteams', data=payload_invalidcmd % 'ack', content_type='application/json', headers=self.headers)
@@ -92,18 +76,20 @@ class MsteamsWebhookTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'Missing/invalid alert_id')
 
         # close alert
-        response = self.client.post('/webhooks/msteams', data=payload_cmd % ('close', alert_id), content_type='application/json', headers=self.headers)
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data['status'], 'ok')
-        self.assertEqual(data['message'], 'status changed')
-        self.assertTrue(bool(response.headers.get('CARD-ACTION-STATUS', False)))
+        # TODO: needs to mock Alert.find_by_id / alert.set_status
+        #response = self.client.post('/webhooks/msteams', data=payload_cmd % ('close', self.alert_id), content_type='application/json', headers=self.headers)
+        #self.assertEqual(response.status_code, 200)
+        #data = json.loads(response.data.decode('utf-8'))
+        #self.assertEqual(data['status'], 'ok')
+        #self.assertEqual(data['message'], 'status changed')
+        #self.assertTrue(bool(response.headers.get('CARD-ACTION-STATUS', False)))
 
         # create blackout
-        response = self.client.post('/webhooks/msteams', data=payload_blackout, content_type='application/json', headers=self.headers)
-        self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data['status'], 'ok')
-        self.assertEqual(data['message'], 'blackout created')
-        self.assertTrue(bool(response.headers.get('CARD-ACTION-STATUS', False)))
+        # TODO: needs to mock: blackout.create()
+        #response = self.client.post('/webhooks/msteams', data=payload_blackout, content_type='application/json', headers=self.headers)
+        #self.assertEqual(response.status_code, 201)
+        #data = json.loads(response.data.decode('utf-8'))
+        #self.assertEqual(data['status'], 'ok')
+        #self.assertEqual(data['message'], 'blackout created')
+        #self.assertTrue(bool(response.headers.get('CARD-ACTION-STATUS', False)))
 
