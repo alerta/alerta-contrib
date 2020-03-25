1ce3dd16b113a48aab9efd38ae9f18802b613d83
Matthieu Serrepuy <lotooo@gmail.com>
2020-03-25 17:02:02 +0100

    Add a statuscake webhook

---
+++ webhooks/statuscake/alerta_statuscake.py
@@ -0,0 +1,41 @@
+from alerta.models.alert import Alert
+from alerta.webhooks import WebhookBase
+from alerta.exceptions import RejectException
+import os
+import hashlib
+
+class StatusCakeWebhook(WebhookBase):
+
+    def incoming(self, query_string, payload):
+        alert_severity = os.environ.get('STATUSCAKE_DEFAULT_ALERT_SEVERITY', 'major')
+
+        # If the statuscake username and apikey are provided
+        # We can validate that the webhook call is valid
+        statuscake_username = os.environ.get('STATUSCAKE_USERNAME')
+        statuscake_apikey = os.environ.get('STATUSCAKE_APIKEY')
+
+        if statuscake_username and statuscake_apikey:
+            decoded_token = statuscake_username + statuscake_apikey
+            statuscake_token = hashlib.md5(decoded_token.encode()).hexdigest()
+            if statuscake_token != payload['Token']:
+                raise RejectException("Provided Token couldn't be verified")
+                
+
+        if payload['Status'] == 'UP':
+            severity = 'normal'
+        else:
+            severity = alert_severity
+
+        return Alert(
+            resource=payload['Name'],
+            event='AppDown',
+            environment='Production',
+            severity=severity,
+            service=['StatusCake'],
+            group='Application',
+            value=payload['StatusCode'],
+            text="%s is down" % payload['URL'],
+            tags=payload['Tags'].split(','),
+            origin='statuscake',
+            raw_data=str(payload)
+        )
