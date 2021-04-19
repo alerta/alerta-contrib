b284f5f06ef32a0f465a7985689aaf57e427d303
Aleksandr Demshin <demshin@gmail.com>
2021-04-19 15:18:29 +0500

    fix routing

--- plugins/routing/alerta_routing.py
+++ plugins/routing/alerta_routing.py
@@ -1,8 +1,5 @@
 def rules(alert, plugins):
-
-    if alert.duplicate_count <= 1:
-        return []
-    elif alert.severity in ['critical', 'major']:
+    if alert.severity in ['critical', 'major']:
         return [plugins['telegram']]
     else:
         return []
