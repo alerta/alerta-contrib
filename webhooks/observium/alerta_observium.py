bea686162dc5fa1598beeb06d0ed777e8a76989e
Iskren Hadzhinedev <iskren.hadzhinedev@x3me.net>
2022-01-14 15:55:38 +0200

    Register exceptions as alerts for easier debugging

--- webhooks/observium/alerta_observium.py
+++ webhooks/observium/alerta_observium.py
@@ -1,6 +1,8 @@
+import traceback
 from alerta.models.alert import Alert
 from alerta.webhooks import WebhookBase
 import json
+import sys
 
 class ObserviumWebhook(WebhookBase):
     """
@@ -8,16 +10,36 @@ class ObserviumWebhook(WebhookBase):
     Implementation by Extreme Labs
     """
     def incoming(self, query_string, payload, path=None):
-
-        return Alert(
-            resource=payload['DEVICE_HOSTNAME'],
-            event=payload['ALERT_MESSAGE'],
-            event_type=payload['ALERT_STATE'],
-            environment='Production',
-            service=['network'],
-            severity=payload['ALERT_SEVERITY'].lower(),
-            value=payload['DEVICE_LOCATION'],
-            text=payload['TITLE'],
-            attributes={},
-            origin='Observium',
-            raw_data=json.dumps(payload))
+        try:
+            return Alert(
+                resource=payload['DEVICE_HOSTNAME'],
+                event=payload['ALERT_MESSAGE'],
+                event_type=payload['ALERT_STATE'],
+                environment='Production',
+                service=['network'],
+                severity=payload['ALERT_SEVERITY'].lower(),
+                value=payload['DEVICE_LOCATION'],
+                text=payload['TITLE'],
+                attributes={},
+                origin='Observium',
+                raw_data=json.dumps(payload))
+        except Exception as e:
+            return Alert(
+                resource='Alerta Observium integration',
+                event='Webhook Failure',
+                event_type='Python Exception',
+                environment='Production',
+                service=['alerta','webhook','observium'],
+                severity='critical',
+                value=str(type(e)),
+                text='Webhook failed',
+                attributes={},
+                origin='Alerta Observium Webhook',
+                raw_data=json.dumps({
+                    'payload': payload,
+                    'query_string': query_string,
+                    'path': path,
+                    'traceback': traceback.format_exc()
+                })
+            )
+            
