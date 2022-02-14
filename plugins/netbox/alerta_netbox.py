c9420cda9a3441758048266c4f07ab239a3720c1
Victor Gorchilov <victor@gorchilov.com>
2022-02-14 12:15:03 +0200

    [up] support python 3.8, error handling

--- plugins/netbox/alerta_netbox.py
+++ plugins/netbox/alerta_netbox.py
@@ -1,6 +1,6 @@
 import logging
 from os import environ
-from typing import Any, Dict, List, MutableMapping, Tuple
+from typing import Any, Callable, Dict, List, MutableMapping, Tuple
 from urllib.parse import urljoin
 
 import requests
@@ -9,28 +9,14 @@ from alerta.plugins import PluginBase
 
 LOG = logging.getLogger("alerta.plugins.netbox")
 
-GQL_QUERY = """
-query FindDevice($q: String) {
-    device_list(q: $q) {
+GQL_QUERY = """query FindDevice($q: String) {{
+    device_list(q: $q) {{
         id
         {fields}
-    }
-}
-"""
+    }}
+}}"""
 
-DEFAULT_FIELDS = """
-serial
-site {
-    name
-    region {
-        name
-    }
-}
-tenant {
-    name
-}
-custom_fields
-"""
+DEFAULT_FIELDS = "site { name, region { name } }, tenant { name }, custom_fields"
 
 
 def flatten(d: MutableMapping, parent_key: str = "", sep: str = "_"):
@@ -55,7 +41,7 @@ class NetboxEnhance(PluginBase):
         NETBOX_TOKEN = environ.get("NETBOX_TOKEN") or kwargs["config"]["NETBOX_TOKEN"]
         NETBOX_FIELDS = (
             environ.get("NETBOX_FIELDS")
-            or kwargs["config"]["NETBOX_FIELDS"]
+            or kwargs["config"].get("NETBOX_FIELDS")
             or DEFAULT_FIELDS
         )
 
@@ -72,7 +58,15 @@ class NetboxEnhance(PluginBase):
             },
         )
 
-        body = res.json()
+        if not res.ok:
+            LOG.error(f"Failed to query Netbox (code {res.status_code}): {res.text}")
+            return alert
+
+        try:
+            body = res.json()
+        except ValueError:
+            LOG.error("Failed to parse response body:", res.text)
+            return alert
 
         if "error" in body or len(body["data"]["device_list"]) == 0:
             LOG.error("Request error:", body)
@@ -85,9 +79,14 @@ class NetboxEnhance(PluginBase):
         device_url = f"{NETBOX_URL}/dcim/devices/{device.pop('id')}"
         device["url"] = f"<a href='{device_url}' target='_blank'>{device_url}</a>"
 
+        trasnform_key: Callable[[str], str] = (
+            lambda x: x[:-4].replace("_", " ")
+            if x.endswith("name") and x != "name"
+            else x
+        )
         alert.attributes.update(
             {
-                f"Netbox {key.removesuffix('name') if key != 'name' else key}".strip(): value
+                f"Netbox {trasnform_key(key)}".strip(): value
                 for key, value in device.items()
             }
         )
