239a672a29f7d1c02980e4760c475ed6f1d42870
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2022-08-25 23:46:57 +0000

    plugin setup file

---
+++ plugins/salesforce/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name="alerta-salesforce",
+    version=version,
+    description='Alerta plugin for SalesForce',
+    url='https://github.com/alerta/alerta-contrib',
+    license='MIT',
+    author='Michael Chambers',
+    author_email='mchambers@mirantis.com',
+    packages=find_packages(),
+    py_modules=['alerta_salesforce'],
+    install_requires=[
+        'simple-salesforce'
+    ],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'alerta.plugins': [
+            'salesforce = alerta_salesforce:OpenCase'
+        ]
+    }
+)
