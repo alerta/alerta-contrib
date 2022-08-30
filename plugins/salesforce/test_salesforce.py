c9e2c71ac032d4efc1cd3aac2e8f917e63005ba6
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2022-08-30 09:46:44 -0700

    first stab at testing file

---
+++ plugins/salesforce/test_salesforce.py
@@ -0,0 +1,55 @@
+import contextlib
+import os
+import unittest
+
+from alerta.app import create_app, plugins
+from alerta_salesforce import SFIntegration, SalesforceClient
+
+@contextlib.contextmanager
+def mod_env(*remove, **update):
+    """
+    See https://stackoverflow.com/questions/2059482#34333710
+
+    Temporarily updates the ``os.environ`` dictionary in-place.
+
+    The ``os.environ`` dictionary is updated in-place so that the modification
+    is sure to work in all situations.
+
+    :param remove: Environment variables to remove.
+    :param update: Dictionary of environment variables and values to add/update.
+    """
+    env = os.environ
+    update = update or {}
+    remove = remove or []
+
+    # List of environment variables being updated or removed.
+    stomped = (set(update.keys()) | set(remove)) & set(env.keys())
+    # Environment variables and values to restore on exit.
+    update_after = {k: env[k] for k in stomped}
+    # Environment variables and values to remove on exit.
+    remove_after = frozenset(k for k in update if k not in env)
+
+    try:
+        env.update(update)
+        [env.pop(k, None) for k in remove]
+        yield
+    finally:
+        env.update(update_after)
+        [env.pop(k) for k in remove_after]
+
+class SalesForcePluginTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_sf_plugin(self):
+
+        test_config = {
+            'TESTING': True,
+            'AUTH_REQUIRED': False
+        }
+
+        self.app = create_app(test_config)
+        self.client = self.app.test_client()
+
+        plugins.plugins['salesforce'] = SFIntegration()
