6eb65c522a6583235dfe3911abe09f76a0dd0f7c
Juan Kungfoo de la Camara <juanmatias@gmail.com>
2024-07-23 09:39:49 -0300

    added falco webhook

---
+++ webhooks/falco/alerta_falco.py
@@ -0,0 +1,106 @@
+from flask import current_app
+
+from alerta.app import alarm_model
+from alerta.models.alert import Alert
+
+from alerta.webhooks  import WebhookBase
+
+class FalcoWebhook(WebhookBase):
+    """
+    Falco webhook
+    """
+
+    def incoming(self, query_string, payload):
+
+        additional_tags = []
+        additional_attributes = {}
+
+        # checking fields
+        #
+        expected_fields = ['priority', 'hostname', 'rule', 'output_fields', 'source', 'output', 'tags']
+        for field in expected_fields:
+            if not field in payload:
+                raise Exception(f'{field} not found in payload')
+        expected_fields_in_outputfields = ['environment']
+        for field in expected_fields_in_outputfields:
+            if not field in payload['output_fields']:
+                raise Exception(f'{field} not found in payload->output_fields')
+
+        # resource+event
+        #
+        # these are the fields used for de duplication, so we fill their values here
+        resource=f"{payload['hostname']}"
+        event = payload['rule']
+
+        # priority
+        #
+        # falco priorities: emergency, alert, critical, error, warning, notice, informational, debug
+        if payload['priority'].lower() in ['emergency', 'alert', 'critical', 'error']:
+            severity = 'critical'
+        elif payload['priority'].lower() in ['notice', 'informational', 'debug']:
+            severity = 'warning'
+        else:
+            severity = alarm_model.DEFAULT_NORMAL_SEVERITY
+        additional_attributes['falco_priority'] = payload['priority']
+
+        # environment
+        #
+        # we set a custom field environment in our setup
+        environment = current_app.config['DEFAULT_ENVIRONMENT']
+        if 'output_fields' in payload and 'environment' in payload['output_fields']:
+            environment = payload['output_fields']['environment']
+
+        # attributes
+        #
+        attributes = additional_attributes
+
+        # tags
+        if not type(payload['tags']) == list:
+            raise Exception('tags should be a list')
+        tags = additional_tags.extend(payload['tags'])
+
+        # group
+        #
+        # how to group
+        group = f"{payload['rule']}-{payload['source']}"
+
+        # value
+        #
+        value = payload['output']
+
+        # service
+        #
+        # service is a List
+        service = [ payload['source'] ]
+
+        # origin
+        #
+        origin = f"{payload['hostname']}-{payload['source']}"
+
+        # event type
+        #
+        # in this case is a Falco Alert
+        event_type = 'falcoAlert'
+
+        # text
+        #
+        # the alert text
+        text = f"{severity}: {payload['output_fields']}"
+
+        return Alert(
+            # alerta will group by these
+            resource=resource,
+            event=event,
+            # ################
+            environment=environment,
+            severity=severity,
+            service=service,
+            group=group,
+            value=value,
+            text=text,
+            tags=tags,
+            origin=origin,
+            attributes=attributes,
+            event_type=event_type,
+            raw_data=payload
+            )
---
+++ webhooks/falco/test_falco.py
@@ -0,0 +1,120 @@
+import json
+import unittest
+
+import alerta_falco
+from alerta.app import create_app, custom_webhooks
+
+
+class FalcoWebhookTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        test_config = {
+            'TESTING': True,
+            'AUTH_REQUIRED': False
+        }
+        self.app = create_app(test_config)
+        self.client = self.app.test_client()
+        custom_webhooks.webhooks['falco'] = alerta_falcowebhook.FalcoWebhook(
+        )
+
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+
+        self.alert_id = 'f0c55228-c61d-462a-9aeb-f6048d37fdf6'
+
+    def test_falcowebhook(self):
+
+        payload_cmd = """
+        {
+           "uuid": "06f73663-b1d4-42e4-b236-eacbd2b96998",
+           "output": "20:39:17.500016161: Notice A shell was spawned in a container with an attached terminal (evt_type=execve user=root user_uid=0 user_loginuid=-1 process=sh proc_exepath=/bin/busybox parent=containerd-shim command=sh -c uptime terminal=34816 exe_flags=EXE_WRITABLE container_id=9273d0110c4e container_image=<NA> container_image_tag=<NA> container_name=<NA> k8s_ns=<NA> k8s_pod_name=<NA>)",
+           "priority": "Notice",
+           "rule": "Terminal shell in container",
+           "time": "2024-07-16T20:39:17.500016161Z",
+           "output_fields": {
+             "container.id": "9273d0110c4e",
+             "container.image.repository": "None",
+             "container.image.tag": "None",
+             "container.name": "None",
+             "evt.arg.flags": "EXE_WRITABLE",
+             "evt.time": 1721162357500016161,
+             "evt.type": "execve",
+             "k8s.ns.name": "None",
+             "k8s.pod.name": "None",
+             "proc.cmdline": "sh -c uptime",
+             "proc.exepath": "/bin/busybox",
+             "proc.name": "sh",
+             "proc.pname": "containerd-shim",
+             "proc.tty": 34816,
+             "user": "jdelacamara",
+             "user.loginuid": -1,
+             "user.name": "root",
+             "user.uid": 0
+           },
+           "source": "syscall",
+           "tags": [
+             "T1059",
+             "container",
+             "maturity_stable",
+             "mitre_execution",
+             "shell"
+           ],
+           "hostname": "nixos"
+         }
+        """
+
+        # Missing alert_id
+        payload_invalidcmd = """
+        {
+           "uuid": "06f73663-b1d4-42e4-b236-eacbd2b96998",
+           "time": "2024-07-16T20:39:17.500016161Z",
+           "output_fields": {
+             "container.id": "9273d0110c4e",
+             "container.image.repository": "None",
+             "container.image.tag": "None",
+             "container.name": "None",
+             "evt.arg.flags": "EXE_WRITABLE",
+             "evt.time": 1721162357500016161,
+             "evt.type": "execve",
+             "k8s.ns.name": "None",
+             "k8s.pod.name": "None",
+             "proc.cmdline": "sh -c uptime",
+             "proc.exepath": "/bin/busybox",
+             "proc.name": "sh",
+             "proc.pname": "containerd-shim",
+             "proc.tty": 34816,
+             "user": "jdelacamara",
+             "user.loginuid": -1,
+             "user.name": "root",
+             "user.uid": 0
+           },
+           "source": "syscall",
+           "tags": [
+             "T1059",
+             "container",
+             "maturity_stable",
+             "mitre_execution",
+             "shell"
+           ],
+           "hostname": "nixos"
+         }
+        """
+
+        # ack with missing fields
+        response = self.client.post('/webhooks/falco', data=payload_invalidcmd %
+                                    'ack', content_type='application/json', headers=self.headers)
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data['status'], 'error')
+        self.assertEqual(data['message'], 'Missing/invalid alert_id')
+
+        # ack with bogus alert_id
+        response = self.client.post('/webhooks/falco', data=payload_cmd % (
+            'ack', '7a0e3ee1-fbaa-45th-isis-bogus'), content_type='application/json', headers=self.headers)
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data['status'], 'error')
+        self.assertEqual(data['message'], 'Missing/invalid alert_id')
+
