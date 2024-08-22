a3aa6f91311cdc930470b2fb5c171b9a4d8164ef
Diego OJ <diego.ojeda@binbash.com.ar>
2024-08-22 18:43:57 -0300

    Fix plugin metadata

--- plugins/remapper/setup.py
+++ plugins/remapper/setup.py
@@ -5,11 +5,11 @@ version = '1.0.0'
 setup(
     name='alerta-remapper',
     version=version,
-    description='Alerta plugin for alert enhancement',
+    description='Alerta plugin for alert fields remapping',
     url='https://github.com/alerta/alerta-contrib',
     license='MIT',
     author='OJ',
-    author_email='oj@example.com',
+    author_email='diego.ojeda@binbash.com.ar',
     packages=find_packages(),
     py_modules=['alerta_remapper'],
     include_package_data=True,
