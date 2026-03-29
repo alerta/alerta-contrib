c835b8f7310a66068445c1479d995fd2ef42d5aa
Faizan Saleem <61307143+faizansaleem-dev@users.noreply.github.com>
2026-03-29 13:10:28 -0230

    [Issue-Fix] jinja2.ext.autoescape extension has been deprecated and removed in Jinja2 3.1 (#408)

--- integrations/mailer/mailer.py
+++ integrations/mailer/mailer.py
@@ -170,7 +170,6 @@ class MailSender(threading.Thread):
         self._subject_template = jinja2.Template(OPTIONS['mail_subject'])
         self._template_env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(self._template_dir),
-            extensions=['jinja2.ext.autoescape'],
             autoescape=True
         )
         if OPTIONS['mail_template_html']:
